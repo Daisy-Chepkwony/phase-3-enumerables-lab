35eecca3d284122bde174a7b67cb80c14f4650ad
+ <+>
2023-02-08 22:48:20 +0300

    completed assignment

--- enumerables.rb
+++ enumerables.rb
@@ -21,27 +21,26 @@ def spiciest_foods(spicy_foods)
   spicy_foods.select{|food|food[:heat_level]>5}
 
 end
-average_heat_level(spicy_foods)
+# average_heat_level(spicy_foods)
 
 # given an array of spicy foods, **output to the terminal**
 # each spicy food in the following format: 
 # Buffalo Wings (American) | Heat Level: 🌶🌶🌶
 # HINT: you can use * with a string to produce the correct number of 🌶 emoji. 
 # "hello" * 3 == "hellohellohello"
+
 def print_spicy_foods(spicy_foods)
-  # your code here
   spicy_foods.each do |food|
-  puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: 🌶" * food[:heat_level]
-   binding.pry
+    heat_level = "🌶" * food[:heat_level]
+    puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{heat_level}"
   end
 end
-print_spicy_foods(spicy_foods)
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
   # your code here
-  spicy_foods.filter { |food| food[:cuisine] == cuisine }  
+  spicy_foods.find { |food| food[:cuisine] == cuisine }  
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
@@ -49,7 +48,7 @@ end
 def sort_by_heat(spicy_foods)
   # your code here
   spicy_foods.sort_by{|food|food[:heat_level]}
-  binding.pry
+  # binding.pry
 end
 # given an array of spicy foods, output to the terminal ONLY 
 # the spicy foods that have a heat level greater than 5, in the following format: 
@@ -59,8 +58,11 @@ def print_spiciest_foods(spicy_foods)
   # your code here
     spiciest_foods = sort_by_heat(spicy_foods).reverse.select { |food| food[:heat_level] > 5 }
     spiciest_foods.each do |food|
-      binding.pry
-      puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: 🌶" * food[:heat_level]
+      heat_level = "🌶" * food[:heat_level]
+      puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{heat_level}"
+    
+     
+      
   end  
 end
 
@@ -70,5 +72,5 @@ def average_heat_level(spicy_foods)
   # your code here
     total_heat = spicy_foods.sum { |food| food[:heat_level] }
     (total_heat.to_f / spicy_foods.count).round
-    binding.pry
+    # binding.pry
 end
